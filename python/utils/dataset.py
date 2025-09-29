5b1a909a6fb18c3c9fbc613c182fae8c530234c2
okushnarev <styx.inc4@gmail.com>
2025-09-29 15:00:14 +0900

    Add info export in create_sequences for evaluation purposes

--- python/utils/dataset.py
+++ python/utils/dataset.py
@@ -6,6 +6,7 @@ from torch.utils.data import Dataset
 
 class SequentialTabularDataset(Dataset):
     'Custom Dataset for sequences of tabular data.'
+
     def __init__(self, features, labels, device='cpu'):
         self.features = features
         self.labels = labels
@@ -17,14 +18,14 @@ class SequentialTabularDataset(Dataset):
     def __getitem__(self, idx):
         # Return a sequence and its corresponding single label
         return torch.tensor(self.features[idx], dtype=torch.float32, device=self.device), \
-               torch.tensor(self.labels[idx], dtype=torch.long, device=self.device)
+            torch.tensor(self.labels[idx], dtype=torch.long, device=self.device)
 
 
-
-def create_sequences(df, group_cols, feature_cols, target_col, sequence_length):
+def create_sequences(df, group_cols, feature_cols, target_col, sequence_length, info_cols=None):
     'Groups data and creates sliding window sequences.'
     sequences = []
     labels = []
+    infos = []
 
     # Group by the columns that define an experiment
     grouped = df.groupby(group_cols)
@@ -36,14 +37,22 @@ def create_sequences(df, group_cols, feature_cols, target_col, sequence_length):
 
         feature_data = group[feature_cols].values
         label_data = group[target_col].values
+        info_data = group[info_cols].values if info_cols else None
 
         # Use a sliding window to create sequences
         for i in range(len(group) - sequence_length + 1):
-            seq = feature_data[i : i + sequence_length]
+            seq = feature_data[i: i + sequence_length]
             # The label for the sequence is the label of its last element
             label = label_data[i + sequence_length - 1]
 
             sequences.append(seq)
             labels.append(label)
 
-    return np.array(sequences), np.array(labels)
+            if info_cols:
+                info = info_data[i + sequence_length - 1]
+                infos.append(info)
+
+    if info_cols:
+        return np.array(sequences), np.array(labels), np.array(infos)
+    else:
+        return np.array(sequences), np.array(labels)
