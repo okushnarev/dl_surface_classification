bac27e5e88669a74855c2fea2fcf7d94661756f7
okushnarev <styx.inc4@gmail.com>
2025-11-19 20:00:07 +0900

    Add generalized optimizer runner
    - Use '--nn_name' to specify NN type, e.g., RNN, CNN.

---
+++ python/run_optimization.py
@@ -0,0 +1,50 @@
+import argparse
+import subprocess
+import sys
+from pathlib import Path
+
+import yaml
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nn_name', type=str, default='rnn', required=True, help='Name of neural network')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    nn_name = args.nn_name
+
+    directory = Path(f'python/{nn_name}/')
+    # Load experiments from the config file
+    with open(directory / 'configs' / 'experiments.yaml', 'r') as f:
+        experiments = yaml.safe_load(f)
+
+    # The rest of the script is almost identical to Option 1
+    for idx, config in enumerate(experiments):
+        print('-' * 50)
+        print(f'Running experiment {idx + 1}/{len(experiments)}: {config["name"]}')
+
+        if 'skip' in config and config['skip']:
+            print('Skipping experiment due to config param "skip"')
+            print('-' * 50)
+            continue
+
+        # Build the command
+        command = [sys.executable, directory / f'optimize_{nn_name}.py']
+        for key, value in config.items():
+            if key in ('optimization', 'common'):
+                # Parse inner dictionaries
+                for k, v in value.items():
+                    command.extend([f'--{k}', str(v)])
+
+        print(' '.join([str(c) for c in command]))
+        print('-' * 50)
+        try:
+            subprocess.run(command, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f'Error running experiment {config['name']}. Stopping.')
+            break
+
+    print('\nAll experiments completed!')
