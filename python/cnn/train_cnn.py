4baa87a304c6dd1ec6fd36ae5786ae5811d41996
okushnarev <styx.inc4@gmail.com>
2025-11-19 19:45:34 +0900

    Add CNN training

---
+++ python/cnn/train_cnn.py
@@ -0,0 +1,61 @@
+import json
+
+from models.cnn import CNN, CNNLayerConfig, MLPLayerConfig
+from python.cnn.utils import dict_to_configs
+from python.train_seq import train
+
+
+def prep_cnn_cfg(cfg_path, input_dim, num_classes, sequence_length):
+    if cfg_path is not None:
+        with open(cfg_path, 'r') as f:
+            config = json.load(f)['params']
+
+        cnn_configs, mlp_configs = dict_to_configs(config)
+
+        start_lr = config['lr']
+    else:
+        cnn_configs = [
+            CNNLayerConfig(out_channels=32, kernel_size=3),
+            CNNLayerConfig(out_channels=64, kernel_size=3),
+        ]
+        mlp_configs = [
+            MLPLayerConfig(out_dim=64, dropout=0.2)
+        ]
+        start_lr = 1e-2
+
+    cfg = dict(
+        model=dict(
+            input_dim=input_dim,
+            num_steps=sequence_length,
+            cnn_configs=cnn_configs,
+            mlp_configs=mlp_configs,
+            num_classes=num_classes,
+        ),
+        optimizer=dict(
+            start_lr=start_lr,
+        )
+    )
+
+    return cfg
+
+
+class CNNTrainWrapper(CNN):
+    def __init__(self,
+                 input_dim: int,
+                 num_steps: int,
+                 cnn_configs: list[CNNLayerConfig],
+                 mlp_configs: list[MLPLayerConfig],
+                 num_classes: int):
+        super().__init__(input_dim, num_steps, cnn_configs, mlp_configs, num_classes)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        return super().forward(x)
+
+
+def main():
+    train(CNNTrainWrapper, prep_cnn_cfg)
+
+
+if __name__ == '__main__':
+    main()
