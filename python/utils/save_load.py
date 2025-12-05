2a2ad3be97de0fee0e06e9ebf0147d224b2647ef
okushnarev <styx.inc4@gmail.com>
2025-12-05 17:11:32 +0700

    Add functionality to save and load cache data in the form of csv + json files

--- python/utils/save_load.py
+++ python/utils/save_load.py
@@ -1,6 +1,8 @@
 import os
-
+import pandas as pd
+from pathlib import Path
 import torch
+import json
 
 
 def save_checkpoint(model, optimizer, epoch, loss, filepath):
@@ -35,3 +37,34 @@ def load_checkpoint(model, optimizer, filepath, device='cpu'):
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
     return checkpoint
+
+
+def save_csv_and_metadata(df: pd.DataFrame, metadata: dict, filepath: Path, **kwargs):
+    """
+    Saves a dataframe and metadata to csv and json
+    :param df: Dataframe to save in csv format
+    :param metadata: Dict metadata to save in json format
+    :param filepath: Path to save csv and json. Pass .csv full path. JSON path will be handlen automatically
+    :param kwargs: Additional arguments to pass to DataFrame.to_csv()
+    """
+    df.to_csv(filepath, **kwargs)
+
+    with open(filepath.with_suffix('.json'), 'w') as file:
+        json.dump(metadata, file, indent=2)
+
+def read_csv_and_metadata(filepath: Path, **kwargs) -> tuple[pd.DataFrame, dict | None]:
+    """
+    Reads a csv file with the corresponding json metadata
+    :param filepath: Path to csv file
+    :param kwargs: Additional arguments to pass to pandas.read_csv()
+    :return: Dataframe and dict metadata. Metadata would be None if json file is not found
+    """
+    df = pd.read_csv(filepath, **kwargs)
+
+    metadata = None
+    if (json_path := filepath.with_suffix('.json')).exists():
+        with open(json_path) as file:
+            metadata = json.load(file)
+
+
+    return df, metadata
