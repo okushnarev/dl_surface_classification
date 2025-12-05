1d1c972ca19b9a1472a640e8f288077eabc66a13
okushnarev <styx.inc4@gmail.com>
2025-12-05 17:10:17 +0700

    Refactor evaluation utils to separate file

---
+++ python/utils/eval_utils.py
@@ -0,0 +1,124 @@
+import pickle
+from dataclasses import dataclass, field
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.preprocessing import LabelEncoder
+from torch.utils.data import DataLoader
+
+from python.utils.dataset import SequentialTabularDataset, create_sequences
+
+
+@dataclass
+class Model:
+    net_type: str  # e.g., RNN, CNN
+    filter_type: str  # e.g., kalman, no_filter
+    dataset: str  # e.g., type_1, type_2
+    model: torch.nn.Module = field(repr=False)
+    features: list[str] = field(repr=False)
+
+    @property
+    def name(self):
+        net_type = self.net_type.upper()
+        filter_type = self.filter_type.replace('_', ' ')
+        dataset = self.dataset.replace('_', ' ')
+        return f'{net_type} {filter_type} {dataset}'
+
+
+def top_sorted_dict(d: dict[str, float], top_n: int, nets: list[str]) -> dict[str, float]:
+    if top_n < 0:
+        return d
+
+    cnt = dict()
+    new_dict = dict()
+    nets_names_ordered = sorted(nets, key=len, reverse=True)
+    for name, acc in d.items():
+        # find net type
+        net_type = ''
+        for n in nets_names_ordered:
+            if n in name:
+                net_type = n
+                break
+
+        # leave top elements only
+        if net_type not in cnt:
+            cnt[net_type] = 0
+        if cnt[net_type] < top_n:
+            new_dict[name] = acc
+        cnt[net_type] += 1
+    return new_dict
+
+
+def run_inference(
+        model_name: str,
+        model_wrapper: Model,
+        df: pd.DataFrame,
+        group_cols: str | list[str],
+        target_col: str,
+        sequence_len: int,
+        info_cols: list[str],
+        label_encoder: LabelEncoder,
+        device: str,
+        batch_size: int,
+        exps_to_unscale: list[str],
+        unscale_cols: list[str]
+) -> pd.DataFrame:
+    """
+    Runs inference for a specific model on a dataframe.
+    Handles scaling, sequence creation, batched inference, and inverse transformation.
+    """
+    print(f'Running {model_name}')
+
+    # Scaler
+    ckpt_path = Path('data/runs') / model_name
+    scaler_path = ckpt_path / 'scaler.pkl'
+    with open(scaler_path, 'rb') as f:
+        scaler = pickle.load(f)
+
+    # Features
+    feature_cols = model_wrapper.features
+
+    # DataFrame prep
+    df_copy = df.copy()
+    df_copy[feature_cols] = scaler.transform(df_copy[feature_cols])
+
+    # Create sequences
+    X, y, info_arr = create_sequences(df_copy, group_cols, feature_cols, target_col, sequence_len, info_cols)
+
+    info = pd.DataFrame(data=info_arr, columns=info_cols)
+    info['surf'] = info['surf'].astype(np.uint8)
+    info['surf'] = label_encoder.inverse_transform(info['surf'])
+
+    # Create loader
+    eval_dataset = SequentialTabularDataset(X, y)
+    eval_loader = DataLoader(eval_dataset, batch_size=batch_size, shuffle=False)
+
+    # Run tests
+    model = model_wrapper.model
+    predictions = np.array([], dtype=np.uint8)
+    model.eval()
+    with torch.no_grad():
+        for sequences, labels in eval_loader:
+            sequences = sequences.to(device, non_blocking=True)
+            outputs = model(sequences)
+            _, predicted = torch.max(outputs.data, 1)
+
+            predictions = np.hstack([predictions, predicted.cpu().numpy()])
+
+    info['predictions'] = label_encoder.inverse_transform(predictions)
+
+    # Unscale certain columns if needed
+    if model_name in exps_to_unscale:
+        # Create placeholder matrix with the shape that Scaler expects
+        dummy_data = np.zeros((len(info), len(feature_cols)))
+        # Copy data_to_unscale to its corresponding position in the placeholder matrix
+        for _col in unscale_cols:
+            dummy_data[:, feature_cols.index(_col)] = info[_col]
+        dummy_data = scaler.inverse_transform(dummy_data)
+        # Take out only chosen unscaled data from the placeholder matrix
+        for _col in unscale_cols:
+            info[_col] = dummy_data[:, feature_cols.index(_col)]
+
+    return info
