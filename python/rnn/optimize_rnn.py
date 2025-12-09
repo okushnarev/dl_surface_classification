8bdc460bf18e85d8803a7c3e941fab314f30b50d
okushnarev <styx.inc4@gmail.com>
2025-12-09 17:26:28 +0700

    Fix RNN initialization in optimization objective

--- python/rnn/optimize_rnn.py
+++ python/rnn/optimize_rnn.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader
 
 from models.rnn import TabularRNN
 from python.optimize_seq import optimize
+from python.utils.net_utils import MLPLayerConfig
 from python.utils.optimization_utils import run_training_loop
 
 
@@ -17,37 +18,22 @@ def rnn_objective(trial, val_dataset, input_dim, num_steps, num_classes, batch_s
     mlp_n_layers = trial.suggest_int('mlp_n_layers', 1, 4)
     mlp_dims = [2 ** trial.suggest_int(f'mlp_dim_{i}_pow', low=4, high=8) for i in range(mlp_n_layers)]
 
+    encoder_layers = [
+        MLPLayerConfig(out_dim=d, dropout=0.2)
+        for d in mlp_dims
+    ]
+
     # Create DataLoaders for this trial
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
     # Instantiate Model and Optimizer
-    model = TabularRNN(input_dim, mlp_dims, embedding_dim, rnn_hidden_dim, num_classes).to(device)
-    optimizer = AdamW(model.parameters(), lr=lr)
-    scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=5)
-    criterion = nn.CrossEntropyLoss()
-
-    # Training & Validation Loop
-    # Train for a fixed number of epochs for each trial
-    accuracy = 0
-    for epoch in range(epochs):
-        model.train()
-        correct, total = 0, 0
-        epoch_loss = 0
-        for sequences, labels in val_loader:
-            sequences = sequences.to(device)
-            labels = labels.to(device)
-
-            outputs = model(sequences)
-            loss = criterion(outputs, labels)
-            epoch_loss += loss.item()
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+    model = TabularRNN(
+        input_dim=input_dim,
+        encoder_layers=encoder_layers,
+        embedding_dim=embedding_dim,
+        rnn_hidden_dim=rnn_hidden_dim,
+        num_classes=num_classes
+    ).to(device)
 
     accuracy = run_training_loop(
         trial,
