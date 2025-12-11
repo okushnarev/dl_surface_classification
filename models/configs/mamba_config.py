dac2978ef471f6979c9d9ae70b657fc9fbf6d0b9
okushnarev <styx.inc4@gmail.com>
2025-12-11 17:04:05 +0700

    Add MAMBA model and its config

---
+++ models/configs/mamba_config.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Literal
+
+class MambaConfig(BaseModel):
+    # Always the same
+    expand: int = 2
+    d_conv: int = 4
+
+    # Can vary
+    d_model: int
+    d_state: Literal[64, 128] = 64
+    headdim: Literal[64, 128] = 64
