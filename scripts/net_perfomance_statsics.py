c6ef2a5aa2e46acad66264b654d483a8bea0f55e
okushnarev <styx.inc4@gmail.com>
2026-09-25 17:24:55 +0700

    Add arguments

--- scripts/net_perfomance_statsics.py
+++ scripts/net_perfomance_statsics.py
@@ -7,4 +7,6 @@ def parse_args():
     parser.add_argument('--configs', nargs='+', default=['belyaev_kushnarev'], help='Experiment YAML filename')
     parser.add_argument('--subset', type=str, default='test', choices=['test', 'train', 'full'],
                         help='Data subset to evaluate on')
+    parser.add_argument('--baseline_df', type=str, default=None, help='Name of classification report df')
+    parser.add_argument('--output_name', type=str, default=None, help='Name of output file to overwrite default')
     return parser.parse_args()
