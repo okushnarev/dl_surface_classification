b5e56960a6917c4275e0fa677d08b0ee070fcf49
okushnarev <styx.inc4@gmail.com>
2026-09-26 09:13:06 +0700

    Add base results load

--- scripts/net_perfomance_statsics.py
+++ scripts/net_perfomance_statsics.py
@@ -1,4 +1,11 @@
 from argparse import ArgumentParser
+import sys
+from pathlib import Path
+# Add project root to PATH
+project_root = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_root))
+from src.data.manipulation import get_results
+from src.utils.excel import prepare_paths
 
 
 def parse_args():
@@ -10,3 +17,15 @@ def parse_args():
     parser.add_argument('--baseline_df', type=str, default=None, help='Name of classification report df')
     parser.add_argument('--output_name', type=str, default=None, help='Name of output file to overwrite default')
     return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    args.ckpt_type = 'stats'
+    nets = sorted(args.nets, key=len, reverse=True)
+
+    baseline_path, output_path = prepare_paths(args)
+
+    # Process data
+    print('Loading results')
+    first_raw_results = get_results(nets, args.configs, 'last', args.subset)
