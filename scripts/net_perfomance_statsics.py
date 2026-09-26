ef2bfef380e0edcd4a4bd5e4b49cc50ca6c3a153
okushnarev <styx.inc4@gmail.com>
2026-09-26 09:15:05 +0700

    Add accuracy stats calculation

--- scripts/net_perfomance_statsics.py
+++ scripts/net_perfomance_statsics.py
@@ -3,13 +3,15 @@ from argparse import ArgumentParser
 import sys
 from collections import defaultdict
 from pathlib import Path
+import pandas as pd
+
 # Add project root to PATH
 project_root = Path(__file__).resolve().parent.parent
 sys.path.append(str(project_root))
 
 from src.utils.paths import ProjectPaths
 from src.data.manipulation import get_results
-from src.utils.excel import prepare_paths
+from src.utils.excel import extract_stats_from_results,  prepare_paths
 
 
 def parse_args():
@@ -47,3 +49,17 @@ def main():
 
     seeded_raw_results = [get_results(nets, cfg, 'best', args.subset) for cfg in seeded_configs]
     print()
+
+    # Stats
+    long_sheet_name = 'Main'
+    wide_sheet_name = 'Main_Wide'
+    main_df_rows, metrics_dfs = zip(*(
+        extract_stats_from_results(_r, [long_sheet_name, wide_sheet_name])
+        for _r in [first_raw_results] + seeded_raw_results
+    ))
+
+    # Prep main df
+    main_df = pd.DataFrame(list(itertools.chain(*main_df_rows)))
+    main_df_unique_cols = main_df[['Net', 'Feature set', 'Stats']].drop_duplicates()
+    main_df_stats = main_df.groupby(['Net', 'Feature set'])['Accuracy'].agg(['mean', 'std']).reset_index()
+    main_df_stats = main_df_stats.merge(main_df_unique_cols, on=['Net', 'Feature set'], how='left')
